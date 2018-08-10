682e42c12d2690ffdd45270c357d0ff964b5c2b1
the-iron-ryan <ryandougherty25@gmail.com>
2018-08-09 18:59:16 -0700

    fixed xz stuffs

---
+++ Assets/_Scripts/Ship.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public int NumPeople;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+
+	}
+}
--- Assets/_Scripts/ShipPayload.cs
+++ Assets/_Scripts/ShipPayload.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipPayload : MonoBehaviour {
+public class ShipPayload : Ship
+{
 
-    public int NumPeople;
 
 	void Start ()
     {
@@ -13,28 +13,28 @@ public class ShipPayload : MonoBehaviour {
 	
 	void Update ()
     {
-		
+        Debug.Log("Gravity Val: " + GetGravityValue());
 	}
     
     public float GetGravityValue()
     {
         // Current position of the ship
-        Vector2 pos = this.transform.position;
+        Vector3 pos = this.transform.position;
         float x = pos.x;
-        float y = pos.y;
+        float z = pos.z;
 
         // Coordinates of planets
         float x1 = ShipManager.Instance.planet11.transform.position.x;
-        float y1 = ShipManager.Instance.planet12.transform.position.y;
+        float z1 = ShipManager.Instance.planet11.transform.position.z;
         float x2 = ShipManager.Instance.planet22.transform.position.x;
-        float y2 = ShipManager.Instance.planet22.transform.position.y;
+        float z2 = ShipManager.Instance.planet22.transform.position.z;
 
 
         // Calculating value of gravity
-        float xy1_val = ShipManager.Instance.planet11.GravityValue * (x2 - x) / (x2 - x1) + ShipManager.Instance.planet21.GravityValue * (x - x1) / (x2 - x1);
+        float xz1_val = ShipManager.Instance.planet11.GravityValue * (x2 - x) / (x2 - x1) + ShipManager.Instance.planet21.GravityValue * (x - x1) / (x2 - x1);
         float xy2_val = ShipManager.Instance.planet12.GravityValue * (x2 - x) / (x2 - x1) + ShipManager.Instance.planet22.GravityValue * (x - x1) / (x2 - x1);
 
-        float gravity_val = xy1_val * (y2 - y) / (y2 - y1) + xy2_val * (y - y1) / (y2 - y1);
+        float gravity_val = xz1_val * (z2 - z) / (z2 - z1) + xy2_val * (z - z1) / (z2 - z1);
 
         return gravity_val;
     }
