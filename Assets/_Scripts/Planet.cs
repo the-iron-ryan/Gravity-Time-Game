6d378937a6e2696f6f8532795675155520845b34
the-iron-ryan <ryandougherty25@gmail.com>
2018-08-09 17:09:45 -0700

    initial commit

---
+++ Assets/_Scripts/Planet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public float Mass;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
